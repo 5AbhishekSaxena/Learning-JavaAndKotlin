571a60b02c8ea70653c662c0c1e46931db8baf92
Abhishek Saxena <5abhisheksaxena@gmail.com>
2019-07-17 01:18:38 +0530

    Add files via upload

--- src/Java/Fibonacci.java
+++ src/Java/Fibonacci.java
@@ -1,27 +1,33 @@
-package in.abhisheksaxena.java;
+package in.abhisheksaxena.java.dynamic_programming.Fibonacci;
 
 /**
  * Created by Abhishek Saxena on 12-07-2019.
  */
 
+import java.util.Scanner;
+
+import static in.abhisheksaxena.java.Utils.print;
+import static in.abhisheksaxena.java.Utils.println;
+
 public class Fibonacci {
 
-    private static int fibonacci(int n) {
-        if(n<=0)
-            return 0;
-        if (n <= 3) {
-            //System.out.print(1 + " ");
-            return 1;
-        } else {
-            //int num = fibonacci(n - 2) + fibonacci(n - 1);
-            //System.out.print(num + " ");
-            return /*num*/ fibonacci(n - 2) + fibonacci(n - 1);
-        }
-    }
-
     public static void main(String[] args) {
-        System.out.println(fibonacci(0));
-        System.out.println(fibonacci(1));
-        System.out.println(fibonacci(5));
+        println("Enter number: ");
+        int n = new Scanner(System.in).nextInt();
+
+        int[] memo = new int[n + 1];
+
+        memo[0] = 1;
+        memo[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            memo[i] = memo[i - 1] + memo[i - 2];
+        }
+
+        for (int i = 0; i < n + 1; i++)
+            print(memo[i] + " ");
+
+        println();
+        println("Output: " + memo[n]);
     }
 }
